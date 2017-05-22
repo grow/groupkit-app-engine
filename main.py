083c8eb54965286ff762be7e730f5c1e201138ca
Jeremy Weinstein <jeremydw@gmail.com>
2017-05-22 14:59:29 -0700

    Use undocumented Google Groups API instead.

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ import google.auth
 import json
 import logging
 import os
+import requests
 import requests_oauthlib
 
 SCOPE = [
-    'https://www.googleapis.com/auth/drive.readonly.metadata',
+    'https://www.googleapis.com/auth/groups',
     'https://www.googleapis.com/auth/userinfo.email',
 ]
 
@@ -25,6 +26,9 @@ _scheme = os.environ['wsgi.url_scheme']
 _host = os.environ['HTTP_HOST']
 REDIRECT_URI = '{}://{}/oauth2callback'.format(_scheme, _host)
 USER_AGENT = 'groupkit-app-engine/0.1'
+
+# Arbitrary page size.
+GROUPS_API = 'https://discussion.googleapis.com/v1/groups?page_size=10000'
 
 app = flask.Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -46,6 +50,21 @@ def get_user_info(credentials):
 
 
 def is_user_in_group(user_email, group_email, credentials):
+    headers = {}
+    credentials.apply(headers)
+    resp = requests.get(GROUPS_API, headers=headers)
+    content = resp.json()
+    if 'groups' not in content:
+        return False
+    for group in content['groups']:
+        if group.get('email') == group_email:
+            return True
+    return False
+
+
+def is_user_in_group_via_google_drive(user_email, group_email, credentials):
+    # Requires a scope such as:
+    #   https://www.googleapis.com/auth/drive.readonly.metadata
     file_id = get_file_id_for_group(group_email)
     service = discovery.build('drive', 'v3', credentials=credentials)
     req = service.files().get(fileId=file_id)
